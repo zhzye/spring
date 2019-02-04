870f4792188395e8a8b420acc90e8d895980eb14
zhzye <zhzyer@gmail.com>
2019-02-04 17:03:00 +0800

    joinpoint for the advice

--- aspectJ/src/main/java/com/zhzye/aspectj/PersonBeforeAdvice.java
+++ aspectJ/src/main/java/com/zhzye/aspectj/PersonBeforeAdvice.java
@@ -1,12 +1,20 @@
 package com.zhzye.aspectj;
 
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.AfterReturning;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.annotation.Pointcut;
 
 @Aspect
 public class PersonBeforeAdvice {
     @Before(value = "execution(* sa*(..))")
-    public void before() {
-        System.out.println("before注入");
+    public void before(JoinPoint joinPoint) {
+        System.out.println("before注入" + joinPoint);
+    }
+
+    @AfterReturning(value = "execution(* sa*(..))")
+    public void after(JoinPoint joinPoint) {
+        System.out.println("after注入 " + joinPoint);
     }
 }
